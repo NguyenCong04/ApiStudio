9ea3b6fe1bfedee2d2d30b8f16b2f5dae50f4bfe
tritoan24 <toanntph32395@fpt.edu.vn>
2024-03-24 16:17:09 +0700

    tri toan

--- Common/COMMON.js
+++ Common/COMMON.js
@@ -1,5 +1,5 @@
 const uri =
-  "mongodb+srv://Admin:NO9C9knLrjAw6Eda@clusterapi.9nmqe80.mongodb.net/ApiStuido";
+  "mongodb+srv://Admin:oGnxfIpnSdLAFPmj@clusterapi.9nmqe80.mongodb.net/ApiStuido";
 
 module.exports = {
   uri: uri,
