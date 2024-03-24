3039fc5b935e4bbea8b5ada47dacfd078f688b02
tritoan24 <toanntph32395@fpt.edu.vn>
2024-03-24 15:15:02 +0700

    update_service

--- Common/COMMON.js
+++ Common/COMMON.js
@@ -1,5 +1,5 @@
 const uri =
-  "mongodb+srv://Admin:12345@clusterapi.9nmqe80.mongodb.net/ApiStuido";
+  "mongodb+srv://Admin:NO9C9knLrjAw6Eda@clusterapi.9nmqe80.mongodb.net/ApiStuido";
 
 module.exports = {
   uri: uri,
--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 var app = express();
-const port = 3000;
+const port = 3001;
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const RES = require('./Common/COMMON');
+const uri = RES.uri;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 
 const database = require('./config/db');
 const apiJobMobile = require('./routes/apiJob');
+const apiServiceMobile = require('./routes/apiService');
 
 app.use(express.json());
 app.use(logger('dev'))
 
 app.use('/apijob',apiJobMobile)
+app.use('/apiservice',apiServiceMobile)
 database.connect();
 
 
@@ -20,4 +33,5 @@ app.listen(port, () => {
 app.get('/',(rq,rs)=>{
     rs.send('WEB')
 })
+
 module.exports = app;
