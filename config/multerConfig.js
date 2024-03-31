34ef4342c4fe68643455442cc114a9b618d5ec86
tritoan24 <toanntph32395@fpt.edu.vn>
2024-04-01 00:16:12 +0700

    ok

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -1,6 +1,6 @@
-const multer = require("multer");
-const storage  = multer.memoryStorage();
+const multer = require('multer');
+const storage = multer.memoryStorage();
 
+const singleUpload = multer({ storage }).single("file");
 
-export const singleUpload = multer({storage}).single("file");
-
+module.exports = { singleUpload }
